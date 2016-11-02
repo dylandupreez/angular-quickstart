2078de41160e02289e29145701badd25b050280d
Dylan Du Preez <Dylandp@privateproperty.co.za>
2016-11-02 14:43:32 +0200

    Added: CRUD operations

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -10,9 +10,10 @@ import { Hero } from "./hero";
 @Injectable()
 export class HeroService {
     private heroesUrl = "app/heroes";
+    private headers = new Headers({ "Content-Type": "application/json" });
 
     constructor(private http: Http) { }
-    
+
     getHeroes(): Promise<Hero[]> {
         return this.http.get(this.heroesUrl)
             .toPromise()
@@ -22,6 +23,31 @@ export class HeroService {
 
     getHero(id: number): Promise<Hero> {
         return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    create(name:string):Promise<Hero>{
+        return this.http
+        .post(this.heroesUrl,JSON.stringify({name:name}),{headers:this.headers})
+        .toPromise()
+        .then(response => response.json().data)
+        .catch(this.handleError);
+
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http.put(url, JSON.stringify(hero), this.headers)
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    delete(id:number):Promise<void>{
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url,{headers:this.headers})
+        .toPromise()
+        .then(() => null)
+        .catch(this.handleError);
     }
 
     handleError(error: any) {
